d0a200796da637032940ab695228c3d0e597277d
Franklin Kingma <franklin@Pepper.local>
2012-06-29 15:39:35 +0200

    Use a ajax fetch for initial call, hardcoded for now

--- plone/formwidget/contenttree/widget.py
+++ plone/formwidget/contenttree/widget.py
@@ -59,7 +59,6 @@ class Fetch(BrowserView):
                                       view_instance)
 
     def __call__(self):
-
         # We want to check that the user was indeed allowed to access the
         # form for this widget. We can only this now, since security isn't
         # applied yet during traversal.
@@ -138,6 +137,7 @@ class ContentTreeBase(Explicit):
         return self.bound_source.getTermByBrain(brain)
 
     def render_tree(self):
+        import pdb; pdb.set_trace( )
         content = self.context
         if not IAcquirer.providedBy(content):
             content = getSite()
@@ -178,7 +178,22 @@ class ContentTreeBase(Explicit):
                         .click( function () {
                             var parent = $(this).parents("*[id$='-autocomplete']")
                             var window = parent.siblings("*[id$='-contenttree-window']")
-                            window.showDialog();
+                            window.showDialog('%(url)s', %(expandSpeed)d);
+                                $('#%(id)s-contenttree').contentTree(
+                    {
+                        script: '%(url)s',
+                        folderEvent: '%(folderEvent)s',
+                        selectEvent: '%(selectEvent)s',
+                        expandSpeed: %(expandSpeed)d,
+                        collapseSpeed: %(collapseSpeed)s,
+                        multiFolder: %(multiFolder)s,
+                        multiSelect: %(multiSelect)s,
+                        rootUrl: '%(rootUrl)s'
+                    },
+                    function(event, selected, data, title) {
+                        // alert(event + ', ' + selected + ', ' + data + ', ' + title);
+                    }
+                );
                         }).insertAfter($(this));
                 });
                 $('#%(id)s-contenttree-window').find('.contentTreeAdd').unbind('click').click(function () {
@@ -188,20 +203,7 @@ class ContentTreeBase(Explicit):
                     $(this).contentTreeCancel();
                 });
                 $('#%(id)s-widgets-query').after(" ");
-                $('#%(id)s-contenttree').contentTree(
-                    {
-                        script: '%(url)s',
-                        folderEvent: '%(folderEvent)s',
-                        selectEvent: '%(selectEvent)s',
-                        expandSpeed: %(expandSpeed)d,
-                        collapseSpeed: %(collapseSpeed)s,
-                        multiFolder: %(multiFolder)s,
-                        multiSelect: %(multiSelect)s,
-                    },
-                    function(event, selected, data, title) {
-                        // alert(event + ', ' + selected + ', ' + data + ', ' + title);
-                    }
-                );
+
         """ % dict(url=url,
                    id=self.name.replace('.', '-'),
                    folderEvent=self.folderEvent,
@@ -210,6 +212,7 @@ class ContentTreeBase(Explicit):
                    collapseSpeed=self.collapseSpeed,
                    multiFolder=str(self.multiFolder).lower(),
                    multiSelect=str(self.multi_select).lower(),
+                   rootUrl='/Plone',
                    name=self.name,
                    klass=self.klass,
                    title=self.title,
