4259d74d3b0749c8a61025abdc73db66e6c87915
Maurits van Rees <maurits@vanrees.org>
2016-02-09 20:45:05 +0100

    Added classifiers in setup.py.

--- setup.py
+++ setup.py
@@ -17,7 +17,11 @@ setup(
     long_description=longdesc,
     classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='Plone selection widget AJAX',
