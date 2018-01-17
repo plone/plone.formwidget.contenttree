9762b960a74dd61eb3003a3aa7a0ee5016e457e9
Maurits van Rees <maurits@vanrees.org>
2018-01-17 13:03:54 +0100

    Preparing release 1.0.16
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '1.0.16.dev0'
+version = '1.0.16'
 desc = 'AJAX selection widget for Plone'
 longdesc = '\n\n'.join([
     open('README.rst').read(),
