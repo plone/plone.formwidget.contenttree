4521402db7548d380280e50bfa18b21a908fcf05
Maurits van Rees <maurits@vanrees.org>
2018-03-07 23:37:15 +0100

    Back to development: 1.1.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-version = '1.1.0'
+version = '1.1.1.dev0'
 desc = 'AJAX selection widget for Plone'
 longdesc = '\n\n'.join([
     open('README.rst').read(),
